bee297f5ab848c927fb4b5e7450414892d0cfb1a
ErdemGura <emraherdemgurallar@gmail.com>
2026-01-28 22:58:50 +0300

    Init
    Init

---
+++ Source/Francesca/Public/FrancescaGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "FrancescaGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRANCESCA_API UFrancescaGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
